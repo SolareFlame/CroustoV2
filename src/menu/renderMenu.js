3a977185b14a6d68249de09df56c4fd33f93fe12
SolareFlame <zeldac332@gmail.com>
2025-01-28 20:22:55 +0100

    🚧 Trying to fix custom config not loading

--- src/menu/renderMenu.js
+++ src/menu/renderMenu.js
@@ -17,13 +17,13 @@ function renderMenu(menu, id) {
         res += "\n";
     }
 
-    if (fs.existsSync("./configs/" + id + ".js")) {
+    if (fs.existsSync(__dirname + "../configs/" + id + ".js")) {
         console.log("Fichier de personnalisation personnalisé trouvé.")
 
-        const { render } = require('./configs/' + id + '.js');
+        const { render } = require(__dirname + "../configs/" + id + ".js");
         res = render(res);
     } else {
-        console.log("Fichier de personnalisation innexistant.")
+        console.log(`Fichier de personnalisation de ${id} innexistant.`)
         console.log("TEST V2.1")
     }
 
