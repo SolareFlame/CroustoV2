c5009d5a22881a27940f8f44c126ef9094a6a2c2
SolareFlame <zeldac332@gmail.com>
2025-01-29 00:39:05 +0100

    🚧 Trying to fix /new issues

--- src/editor/restaurants.js
+++ src/editor/restaurants.js
@@ -35,12 +35,10 @@ function filterRestaurants() {
         !restaurant.title.includes("Facteria")
     );
 
-    if(list.length > 25) {
+    if(list.length > 24) {
         console.log("WARNING: More than 25 restaurants found. Limiting to 25.");
-        list = list.slice(0, 25);
+        list = list.slice(0, 24);
     }
-
-    console.log("Filtered restaurants: ", list);
 
     return list;
 }
